38f17d771de5437d3187b3b7355b533770920938
zoro-wyz <17853463870@163.com>
2019-05-27 22:41:36 +0800

    二分法查找练习

---
+++ src/com/wyz/dome01/BinarySearchTest.java
@@ -0,0 +1,50 @@
+package com.wyz.dome01;
+
+/**
+ * 二分查找练习
+ */
+public class BinarySearchTest {
+
+    public static void main(String[] args) {
+
+        int[] array = {1,2,3,4,5,6,7,8,9};
+
+        //定义开始的下标
+        int begin = 0;
+        //定义结束的下标
+        int end = array.length;
+        //定义中间的下标
+        int mid = (begin + end)/2;
+
+        //目标元素
+        int target = 9;
+        int index = -1;
+        while (true){
+
+            //先判断中间元素
+            if ( target == array[mid] ){
+                index = mid;
+                break;
+
+              //如果不相等，那么根据中间值的大小来改变开始或者结尾值
+            }else {
+
+                //如果中间值大于目标值，则改变结尾值
+                if ( array[mid] > target){
+                    end = mid - 1;
+
+                    //否则，改变开始值
+                }else {
+                    begin = mid + 1;
+                }
+
+                //最后，都要改变中间值
+                mid = (begin + end)/2 ;
+            }
+
+        }
+        System.out.println("index :" + index );
+
+    }
+
+}
